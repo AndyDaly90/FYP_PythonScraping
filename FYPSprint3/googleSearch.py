7d2e2211605ba57cfd353a95b57b98fc9d37a421
AndyDaly90 <andrew.daly@students.ittralee.ie>
2016-02-08 23:30:54 +0000

    Script now producing clean URL's, although code is messy and needs to be cleaned up,

--- FYPSprint3/googleSearch.py
+++ FYPSprint3/googleSearch.py
@@ -14,34 +14,35 @@ def split_links(data):
     return _link
 
 
-# create a browser instance
-browser = mechanize.Browser()
-# I don't want my browser to be seen as a robot.
-browser.set_handle_robots(False)
-#
-browser.addheaders = [('User-Agent', 'Mozilla/5.0')]
-
-make = "audi"
-model = "a4"
-area = "kerry"
-
-sites = ["cbg", "carsIreland", "adverts.ie", "carzone"]
-
-for site in sites:
-    google_request = "http://www.google.ie/search?q=used+%s+%s+%s+%s" % (make, model, site, area)
+def get_links(_make, _model, _site, _area):
+    google_request = "http://www.google.ie/search?q=used+%s+%s+%s+%s" % (_make, _model, _site, _area)
     result = browser.open(google_request).read()
     # Parse Div
     soup = BeautifulSoup(result, 'html.parser')
     search_div = soup.find_all('div', attrs={'id': 'ires'})
     # Container for all links in search result
-    searchText = str(search_div[0])
-    # print(searchText)
+    search_text = str(search_div[0])
 
-    link = BeautifulSoup(searchText, 'html.parser')
+    link = BeautifulSoup(search_text, 'html.parser')
     list_items = link.find_all('a')
-    itemsAsString = str(list_items)
+    item_as_string = str(list_items)
+    return item_as_string
 
-    regex = re.findall('q="?\'?([^"\'>]*)', itemsAsString)
+
+browser = mechanize.Browser()  # create a browser instance
+browser.set_handle_robots(False)  # I don't want my browser to be seen as a robot.
+browser.addheaders = [('User-Agent', 'Mozilla/5.0')]
+
+make = "audi"
+model = "a4"
+area = "kerry"
+sites = ["cbg", "carsIreland", "adverts.ie", "carzone"]
+
+for site in sites:
+    links = get_links(make, model, site, area)
+    regex = re.findall('[a-z]+[:.].*?(?=\s)', links)
     data = split_links(str(regex))
-    url = data[0]
-    print(url)
+    url = data[0]  # Urls contained in first index of result
+
+    clean_url = url[2:]  # Full URL to allow for network requests
+    print(clean_url)
