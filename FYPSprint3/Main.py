dd7973856355531cb7c979ab6723be630505fd0c
AndyDaly90 <andrew.daly@students.ittralee.ie>
2016-04-07 01:02:57 +0100

    Cleaned up code, separation of concerns, scripts are now less coupled and easier to test.
    Fixed Regex for car prices.

---
+++ FYPSprint3/Main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from carCrawler import CarCrawler
+
+if __name__ == '__main__':
+    web_crawler = CarCrawler()
+
+    make = raw_input("Enter Make: ")
+    model = raw_input("Enter Model: ")
+    area = "kerry"
+    sites =  ["carsireland", "cbg"]
+
+    for site in sites:
+        searchResult = web_crawler.perform_google_search(make, model, site, area)
+        dirtyURL = web_crawler.getURL(searchResult)
+        cleanedURL = web_crawler.clean_URL(dirtyURL)
+
+        url = cleanedURL[0]
+        print(url)
+
+        webPage = web_crawler.get_web_page(url)
+        print(webPage.title)
+
+        car_prices = web_crawler.get_car_prices(webPage)
+        web_crawler.get_car_info(webPage)
+
