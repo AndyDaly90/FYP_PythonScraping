1b3a790d5f1f24c14edb4013639b0c8fc56042f0
Andrew Daly <andrew.daly@students.ittralee.ie>
2016-04-13 00:01:39 +0100

    changed make model to command line arguments, easier for deployment.

--- FYPSprint3/Main.py
+++ FYPSprint3/Main.py
@@ -1,11 +1,14 @@
+import sys
 # -*- coding: utf-8 -*-
 from carCrawler import CarCrawler
 
 if __name__ == '__main__':
     web_crawler = CarCrawler()
 
-    make = raw_input("Enter Make: ")
-    model = raw_input("Enter Model: ")
+   # make = raw_input("Enter Make: ")
+   # model = raw_input("Enter Model: ")
+    make = sys.argv[1]
+    model = sys.argv[2]
     area = "kerry"
     sites = ["carsIreland", "cbg", "donedeal", "adverts", "beepbeep"]
 
